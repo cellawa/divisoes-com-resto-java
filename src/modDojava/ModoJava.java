7301113f22d9f708c9da14392a0a391173876fe8
louko <w.riosmagalhaes@gmail.com>
2022-08-12 10:36:59 -0300

    Value da media Matematica

--- src/modDojava/ModoJava.java
+++ src/modDojava/ModoJava.java
@@ -5,18 +5,28 @@ import javax.swing.JOptionPane;
 public class ModoJava {
 
 	public static void main(String[] args) {
-		/* Modolo resto da divisão */
 
-		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros");
-		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas");
-		double carroNumero = Double.parseDouble(carros);
-		double pessoaNumero = Double.parseDouble(pessoas);
-		int divisao = (int) (carroNumero / pessoaNumero);
-		
-		double resto = carroNumero % pessoaNumero;
-		JOptionPane.showMessageDialog(null, "Divisao para pessoas deu " + divisao + " carros e sobrou " + resto +" carros");
+		String nota1 = JOptionPane.showInputDialog("Informe a nota 1");
+		String nota2 = JOptionPane.showInputDialog("Informe a nota 2");
+		String nota3 = JOptionPane.showInputDialog("Informe a nota 3");
+		String nota4 = JOptionPane.showInputDialog("Informe a nota 4");
 
+		double dNota1 = Double.parseDouble(nota1);
+		double dNota2 = Double.parseDouble(nota2);
+		double dNota3 = Double.parseDouble(nota3);
+		double dNota4 = Double.parseDouble(nota4);
+		double media = (dNota1 + dNota2 + dNota3 + dNota4) / 4;
+		/* Media para aprovaçao é 70 */
+		if (media >= 50) {
+			if (media >= 70) {
+				JOptionPane.showMessageDialog(null, "Aluno está aprovado com a média de " + media + " parabéns");
 
+			} else {
+					JOptionPane.showMessageDialog(null, "Aluno está de recuperação " + media + " estude mais vagabundo");
+		} 
 
+	}else {
+		JOptionPane.showMessageDialog(null, "Aluno está reporvado " + media + " esse e o verdadeiro vagabundoooo");
 	}
+}
 }
